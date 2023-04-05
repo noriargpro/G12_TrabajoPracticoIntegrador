dd3f8728d93d1f805dd1e03546ad1c6215b0959a
marianaPerez22 <marianper543@gmail.com>
2023-04-05 02:41:24 -0300

    agregueButton

--- main.js
+++ main.js
@@ -1,17 +1,52 @@
+document.addEventListener("DOMContentLoaded",() =>{
+    var siguiente = document.getElementById("siguiente");
+    siguiente.addEventListener("click",()=>{
+    var contenedorEjercicio = document.getElementById("contenedorEjercicio");  
+
+     var elementoActivo = contenedorEjercicio.querySelector(".activo");
+     var elementoSiguiente = contenedorEjercicio.querySelector(".activo + .desactivo");
+    
+     if(elementoSiguiente === null){ 
+         elementoSiguiente = contenedorEjercicio.querySelector(".desactivo:first-of-type");
+     }
+
+    elementoActivo.classList.remove("activo");
+    elementoActivo.classList.add("desactivo");
+    
+    elementoSiguiente.classList.remove("desactivo");
+    elementoSiguiente.classList .add("activo");
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    var button = document.getElementById("miButton");
-    button.addEventListener("click", () => {
-        var contenedorEjercicio = document.getElementById("contenedorEjercicio");    
-       
-        
-        /* SPLIT */
-        var elementoActivo = contenedorEjercicio.querySelector(".activo");
-        var idElementoActivo = elementoActivo.id;
-        var descomposicionID = idElementoActivo.split("-");
-        var numeroElementoSiguiente = Number(descomposicionID[1]) + 1;
-        var elementoSiguiente = contenedorEjercicio.querySelector("#elemento-" + numeroElementoSiguiente);
-        console.log(elementoSiguiente);
-    });
+    }); 
+  
+
+
+    var anterior = document.getElementById("anterior");
+
+    anterior.addEventListener("click",()=>{
+        var contenedorEjercicio = document.getElementById("contenedorEjercicio");  
+           
+        var elementosDiv = contenedorEjercicio.querySelectorAll(".activo , .desactivo");
+   
+
+         for (var i= 0; i< elementosDiv.length; i++) {
+          
+          if (elementosDiv[i].classList.contains("activo") === true) {
+            break;
+           
+          }
+      
+         }
+
+         var posicionAnterior = i-1;
+         if(posicionAnterior === -1){
+            posicionAnterior = elementosDiv.length-1;
+         }
+    
+         elementosDiv[i].classList.remove("activo");
+         elementosDiv[i].classList.add("desactivo");
+
+         elementosDiv[posicionAnterior].classList.remove("desactivo");
+         elementosDiv[posicionAnterior].classList.add("activo");
+});
 });
