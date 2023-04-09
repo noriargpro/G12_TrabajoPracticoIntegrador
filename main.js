2ff92ff4a1b1bb2c74359b592be4637c4746cb44
marianaPerez22 <marianper543@gmail.com>
2023-04-09 12:18:56 -0300

    formularioInscripcion.js

--- main.js
+++ main.js
@@ -50,3 +50,29 @@ document.addEventListener("DOMContentLoaded",() =>{
          elementosDiv[posicionAnterior].classList.add("activo");
 });
 });
+//***************formulario de inscripcion ***************** */
+const nextBtn = document.querySelector("#next-btn");
+const backBtn = document.querySelector("#back-btn");
+const step1 = document.querySelector("#step-1");
+const step2 = document.querySelector("#step-2");
+const summary = document.querySelector("#summary");
+
+//agregar evento onclick al botón siguiente
+nextBtn.addEventListener("click", function() {
+  // recopilar datos del primer paso
+  let name = document.querySelector("#name").value;
+  let email = document.querySelector("#email").value;
+  let apellido = document.querySelector("#apellido").value;
+  let materia = document.querySelector("#materia").value;
+  
+  // mostrar resumen en el segundo paso
+  summary.innerHTML = `Nombre: ${name}<br>Apellido: ${apellido}<br>Materia: ${materia}<br> Email: ${email}`; 
+
+
+     
+  // ocultar el primer paso y mostrar el segundo paso
+   step1.style.display = "none";
+   step2.style.display = "block";
+});
+
+/*************************** */
